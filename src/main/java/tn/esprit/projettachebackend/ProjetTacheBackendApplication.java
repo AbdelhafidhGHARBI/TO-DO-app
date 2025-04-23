1c3581eb096dca124e5358c493209aeac08727ab
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2025-04-23 22:31:08 +0100

    first commit: to do app one component

---
+++ src/main/java/tn/esprit/projettachebackend/ProjetTacheBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.projettachebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTacheBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetTacheBackendApplication.class, args);
+	}
+
+}
